3ecf78e1ea2284e198c10b1910bd5043bc5deebf
Brian Rogers <btcr1990@gmail>
2015-06-01 12:27:27 -0500

    Removed URL

--- announcements/BB_-_Announcemnt_-_Not_Date_Restricted.user.js
+++ announcements/BB_-_Announcemnt_-_Not_Date_Restricted.user.js
@@ -2,16 +2,8 @@
 // @name        BB - Announcemnt - Not Date Restricted
 // @namespace   channel1450
 // @description Changes duration to Not Date Restricted
-<<<<<<< HEAD
 // @include     
 // @version     1
 // @grant       none
 // ==/UserScript==
 document.getElementById("isPermanent_true").checked = true;
-=======
-// @include     https://bb.uis.edu/webapps/blackboard/execute/announcement?blackboard.platform.security.NonceUtil.nonce=*&method=add&viewChoice=2&editMode=true&tabAction=false&announcementId=&course_id=*&context=course&internalHandle=cp_announcements&searchSelect=*
-// @version     1
-// @grant       none
-// ==/UserScript==
-document.getElementById("isPermanent_true").checked = true;
->>>>>>> 039a9d5da7847f0a6bc382bdb2c0985272f8d5e4
