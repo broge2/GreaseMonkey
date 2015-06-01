db2a3b001f07c2a13d30ccae10343faa685a0926
Brian Rogers <btcr1990@gmail>
2015-06-01 12:19:37 -0500

    Initial Commit

---
+++ announcements/BB_-_Announcemnt_-_Not_Date_Restricted.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        BB - Announcemnt - Not Date Restricted
+// @namespace   channel1450
+// @description Changes duration to Not Date Restricted
+// @include     
+// @version     1
+// @grant       none
+// ==/UserScript==
+document.getElementById("isPermanent_true").checked = true;
