b250643a4dc7e8fb97c5b1770ad469645df6ab42
Brian Rogers <btcr1990@gmail>
2015-06-21 00:56:55 -0500

    Added script to change needs_grading image

---
+++ grading/Needs_Grading_Image.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Needs Grading Image
+// @namespace   channel1450
+// @include     https://bb.uis.edu/webapps/gradebook/do/instructor/enterGradeCenter?course_id=_16103_1&cvid=122545
+// @version     1
+// @grant       none
+// ==/UserScript==
+gridImages["needsGrading"] = '<img src=\'http://www.rw-designer.com/icon-image/4829-18x18x4.png\' alt=\'Needs Grading\' title=\'Needs Grading\'>'
