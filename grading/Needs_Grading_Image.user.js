525bc4a5509b82fc1c661e6e3ab043602257bec6
Brian Rogers <btcr1990@gmail>
2015-06-21 00:58:43 -0500

    Changed hardcoded src and removed link

--- grading/Needs_Grading_Image.user.js
+++ grading/Needs_Grading_Image.user.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name        Needs Grading Image
 // @namespace   channel1450
-// @include     https://bb.uis.edu/webapps/gradebook/do/instructor/enterGradeCenter?course_id=_16103_1&cvid=122545
 // @version     1
 // @grant       none
 // ==/UserScript==
-gridImages["needsGrading"] = '<img src=\'http://www.rw-designer.com/icon-image/4829-18x18x4.png\' alt=\'Needs Grading\' title=\'Needs Grading\'>'
+gridImages["needsGrading"] = '<img src=\'Link to Image Here\' alt=\'Needs Grading\' title=\'Needs Grading\'>'
